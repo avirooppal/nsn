b54eadfe6bbc5f32987f3add14b4b838ac278ab3
avirooppal <avirooppal42@gmail.com>
2026-06-15 13:15:20 +0530

    TASK-0020: Create StorageAdapter interface

---
+++ neurosleepnet/storage/__init__.py
@@ -0,0 +1,3 @@
+from .base import StorageAdapter
+
+__all__ = ["StorageAdapter"]
---
+++ neurosleepnet/storage/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class StorageAdapter(ABC):
+    """
+    Abstract interface for all storage adapters.
+    """
+    
+    @abstractmethod
+    def store(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def get(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def delete(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def list(self, *args, **kwargs):
+        pass
