dafd665325ddbdee8a56cabe1fdf648a9a267cee
avirooppal <avirooppal42@gmail.com>
2026-06-15 13:21:38 +0530

    TASK-0023: Store memory record

--- neurosleepnet/storage/sqlite.py
+++ neurosleepnet/storage/sqlite.py
@@ -23,8 +23,15 @@ class SQLiteAdapter(StorageAdapter):
         conn.commit()
         conn.close()
 
-    def store(self, *args, **kwargs):
-        pass
+    def store(self, memory_id: str, content: str, created_at: str):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO memories (id, content, created_at)
+            VALUES (?, ?, ?)
+        ''', (memory_id, content, created_at))
+        conn.commit()
+        conn.close()
 
     def get(self, *args, **kwargs):
         pass
